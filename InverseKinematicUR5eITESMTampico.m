638a9c603512bb3ee21e7b242fdae3949126f513
IrandiGC <149118292+IrandiGC@users.noreply.github.com>
2024-03-17 22:44:55 -0600

    Update InverseKinematicUR5eITESMTampico.m
    Se ha aumentado la capacidad de detectar y descartar soluciones.

--- InverseKinematicUR5eITESMTampico.m
+++ InverseKinematicUR5eITESMTampico.m
@@ -1,6 +1,7 @@
 function output = InverseKinematicUR5eITESMTampico(H_Actual,q_Anterior)
-    %Unidades en cm    
-    escala = 10;
+    %Unidades en m    
+    escala = 1;
+    
     d_1 = 0.1625*escala;
     d_4 = 0.1333*escala;
     d_5 = 0.0997*escala;
@@ -9,14 +10,21 @@ function output = InverseKinematicUR5eITESMTampico(H_Actual,q_Anterior)
     a_2 = -0.425*escala;
     a_3 = -0.3922*escala;
 
+    %Se prepara una matriz para almacenar las posibles 16 soluciones.
     numSol = 16;
     theta = zeros(6,numSol);
+    P = zeros(3,numSol);
+
+    %Aquí se coloca el algoritmo para generar las 16 soluciones, revisar
+    %artículo
+
     p05x= H_Actual(1,4)-d_6*H_Actual(1,3);
     p05y= H_Actual(2,4)-d_6*H_Actual(2,3);
     theta(1,1:numSol/2)= atan2(p05y,p05x)+acos(d_4/(sqrt(p05x^2+p05y^2)))+pi/2;
     theta(1,numSol/2+1:end)= atan2(p05y,p05x)-acos(d_4/(sqrt(p05x^2+p05y^2)))+pi/2;
 
     for i = 1:16
+        %Si alguna solución númerica no se puede calcular, se descarta y se coloca NAN 
         try
             if mod(i,8)==1
                 p16z= H_Actual(1,4)*sin(theta(1,i))-H_Actual(2,4)*cos(theta(1,i));
@@ -48,17 +56,24 @@ function output = InverseKinematicUR5eITESMTampico(H_Actual,q_Anterior)
             theta(:,i)=NaN;
         end
     end
-    temp0 = sum((theta-q_Anterior).^2,1);
-    output = theta(:,temp0 == min(temp0));
-% T01 = mi_HT(q(1),d_1,0,pi/2);
-% T12 = mi_HT(q(2)-pi/2,0,a_2,0);
-% T23 = mi_HT(q(3),0,a_3,0);
-% T34 = mi_HT(q(4)+pi/2,d_4,0,pi/2);
-% T45 = mi_HT(q(5)-pi/2,d_5,0,-pi/2);
-% T56 = mi_HT(q(6),d_6,0,0);
+    %De todas soluciones, se determina su ubicación final.
+    for i=1:numSol
+        temp0 = mi_HT(theta(1,i),d_1,0,pi/2)*mi_HT(theta(2,i),0,a_2,0)*...
+        mi_HT(theta(3,i),0,a_3,0)*mi_HT(theta(4,i),d_4,0,pi/2)*...
+        mi_HT(theta(5,i),d_5,0,-pi/2)*mi_HT(theta(6,i),d_6,0,0);
+        P(:,i) = temp0(1:3,4);
+    end
+    %Se descartan todas las soluciones que no son soluciones.
+    temp1 = sum((P-H_Actual(1:3,4)).^2,1)
+    theta = theta(:,temp1 < 0.001);
+    %nos quedamos con la solución que esta mas cerca del punto de partida.
+    temp2 = sum((theta-q_Anterior).^2,1)
+    output = theta(:,temp2 == min(temp2));
+
 
 end
 
+% Funciones usadas en este algoritmo.
 function output = mi_HT(theta,d,a,alpha)
     output = [mi_Rotz(theta),[0 0 0]';[0 0 0],1]*...
         [eye(3),[a 0 d]';[0 0 0],1]*...
